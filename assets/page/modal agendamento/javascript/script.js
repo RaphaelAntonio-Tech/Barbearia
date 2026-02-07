e7b23e6aac5e3515678e43a866a55469c3c42cd1
Raphael Tech <raphaelantonio.dev@gmail.com>
2026-02-07 08:12:58 -0300

    update final ajustes

--- assets/page/modal agendamento/javascript/script.js
+++ assets/page/modal agendamento/javascript/script.js
@@ -30,12 +30,58 @@ function agendar() {
     //3. Verificacao de Regras
     const agora = new Date();
     const dataSelecionada = new Date(data.value)
+    const diaAtual = agora.getDate();
+    const mesAtual = agora.getMonth();
+    const anoAtual = agora.getFullYear();
+
 
     if (dataSelecionada < agora) {
         res.style.background = '#b100009c'
         res.style.color = '#fff';
         res.style.padding = '15px';
-        res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${agora}</strong>`
+
+
+        switch (mesAtual) {
+            case 0:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/01/${anoAtual}</strong>`
+                break;
+            case 1:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/02/${anoAtual}</strong>`
+                break;
+            case 2:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/03/${anoAtual}</strong>`
+                break;
+            case 3:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/04/${anoAtual}</strong>`
+                break;
+            case 4:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/05/${anoAtual}</strong>`
+                break;
+            case 5:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/06/${anoAtual}</strong>`
+                break;
+            case 6:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/07/${anoAtual}</strong>`
+            case 7:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/08/${anoAtual}</strong>`
+                break;
+            case 8:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/09/${anoAtual}</strong>`
+                break;
+            case 9:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/10/${anoAtual}</strong>`
+                break;
+            case 10:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/11/${anoAtual}</strong>`
+                break;
+            case 11:
+                res.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Data errada!! Data atual é: <strong>${diaAtual}/12/${anoAtual}</strong>`
+                break;
+            default:
+                res.innerHTML = `[ERROR]`
+                break;
+        }
+
         return;
     }
 
